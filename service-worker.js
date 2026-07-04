d7d10bdc3c78fedf97679f50d649f763159a6b44
lonny-tech <lonnykathurima99@gmail.com>
2026-07-04 08:54:38 -0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,24 @@
+const CACHE_NAME = "lonny-app-v1";
+const urlsToCache = [
+  "index.html",
+  "calculator.html",
+  "motivation.jpg"
+];
+
 self.addEventListener("install", function(event) {
-    console.log("Service Worker Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
 });
